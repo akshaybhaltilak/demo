5a27df35303a72bf7925dcd327c59efbb8669bbb
akshaybhaltilak <99380793+akshaybhaltilak@users.noreply.github.com>
2024-06-20 08:33:34 +0530

    togle key add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,93 +3,196 @@ import {Link, NavLink} from 'react-router-dom'
 
 export default function Header() {
     return (
-        <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Z5bwr3UTmH8pEevGbGCPYPhrEIEDssRTYw&s"
-                            className="mr-3 h-12"
-                            alt="Logo"
-                        />
-                    </Link>
-                    <div className="flex items-center lg:order-2">
-                        <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Call:9834153020
-                        </Link>
-                    </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                to="/"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/about"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/contact"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Contact
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/praposal"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    For Praposal
-                                </NavLink>
-                            </li>
+        // <header className="shadow sticky z-50 top-0">
+        //     <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        //         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        //             <Link to="/" className="flex items-center">
+        //                 <img
+        //                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Z5bwr3UTmH8pEevGbGCPYPhrEIEDssRTYw&s"
+        //                     className="mr-3 h-12"
+        //                     alt="Logo"
+        //                 />
+        //             </Link>
+        //             <div className="flex items-center lg:order-2">
+        //                 <Link
+        //                     to="#"
+        //                     className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+        //                 >
+        //                     Log in
+        //                 </Link>
+        //                 <Link
+        //                     to="#"
+        //                     className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+        //                 >
+        //                     Call:9834153020
+        //                 </Link>
+        //             </div>
+        //             <div
+        //                 className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+        //                 id="mobile-menu-2"
+        //             >
+        //                 <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+        //                     <li>
+        //                         <NavLink
+        //                         to="/"
+        //                             className={({isActive}) =>
+        //                                 `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+        //                             }
+        //                         >
+        //                             Home
+        //                         </NavLink>
+        //                     </li>
+        //                     <li>
+        //                         <NavLink
+        //                         to="/about"
+        //                             className={({isActive}) =>
+        //                                 `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+        //                             }
+        //                         >
+        //                             About
+        //                         </NavLink>
+        //                     </li>
+        //                     <li>
+        //                         <NavLink
+        //                         to="/contact"
+        //                             className={({isActive}) =>
+        //                                 `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+        //                             }
+        //                         >
+        //                             Contact
+        //                         </NavLink>
+        //                     </li>
+        //                     <li>
+        //                         <NavLink
+        //                         to="/praposal"
+        //                             className={({isActive}) =>
+        //                                 `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+        //                             }
+        //                         >
+        //                             For Praposal
+        //                         </NavLink>
+        //                     </li>
 
 
-                            <li>
-                                <NavLink
-                                to="/services"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Services
-                                </NavLink>
-                            </li>
+        //                     <li>
+        //                         <NavLink
+        //                         to="/services"
+        //                             className={({isActive}) =>
+        //                                 `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+        //                             }
+        //                         >
+        //                             Services
+        //                         </NavLink>
+        //                     </li>
                             
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        //                 </ul>
+        //             </div>
+        //         </div>
+        //     </nav>
+        // </header>
+
+
+
+
+
+
+
+
+
+        <header className="shadow sticky z-50 top-0">
+    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <Link to="/" className="flex items-center">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Z5bwr3UTmH8pEevGbGCPYPhrEIEDssRTYw&s"
+                    className="mr-3 h-12"
+                    alt="Logo"
+                />
+            </Link>
+            <div className="flex items-center lg:order-2">
+                <Link
+                    to="#"
+                    className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                >
+                    Log in
+                </Link>
+                <Link
+                    to="#"
+                    className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                >
+                    Call:9834153020
+                </Link>
+                <button
+                    type="button"
+                    className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    aria-controls="mobile-menu-2"
+                    aria-expanded="false"
+                    onClick={() => document.getElementById('mobile-menu-2').classList.toggle('hidden')}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                    </svg>
+                </button>
+            </div>
+            <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/contact"
+                            className={({ isActive }) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/praposal"
+                            className={({ isActive }) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            For Proposal
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/services"
+                            className={({ isActive }) =>
+                                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                            }
+                        >
+                            Services
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
     );
 }
 
